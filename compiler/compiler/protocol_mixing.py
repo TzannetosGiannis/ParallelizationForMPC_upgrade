4410a067b405012c7db00a459696055b6254e9df
Brandon Fogg <foggb@rpi.edu>
2024-09-05 19:24:26 -0400

    Added dictionary of [Constant -> set[Protocol]] to config output to make compilation easier

--- compiler/compiler/protocol_mixing.py
+++ compiler/compiler/protocol_mixing.py
@@ -101,6 +101,7 @@ class Config:
     total_cost: float                       # the total cost (including conversions) required to execute this sequence
     protocolByVar: dict[Var, set[Protocol]] # a quick way to access the protocols of each variable
     finalStmts: list[Statement]             # used to print the return statement and the tuple if it exists
+    constants: dict[int, set[Protocol]]          # dictionary that converts constants to the required protocols
 
     # sets of variables can be locked to the same set of protocols by transparent (zero-cost) operations
     lockedVarsIdx: dict[Var, int]
@@ -115,13 +116,27 @@ class Config:
         self.lockedVarsSets = []
         self.protocolByVar = dict()
         self.finalStmts = []
+        self.constants = dict()
 
     def __str__(self) -> str:
         inputs = "Input vars: {" + ", ".join(str(var) + ": " + (str(conv) if len(conv) else '{}') for var, conv in self.inputs.items()) + "}"
+        constants = "Constants: {" + ", ".join(str(val) + ": " + (str(conv) if len(conv) else '{}') for val, conv in self.constants.items()) + "}"
         stmts = "\n".join("\t"*count + str(stmt) + ": " + str(assign) + " -> " + (str(conv) if len(conv) else '{}') + " for " + "{:.2f}".format(cost) + " * " + str(depth) + " = " + "{:.2f}".format(cost*depth) + (" (" + (", ".join(cd[0] + "->" + cd[1] for cd in convDict) if len(conv) else "") + ")" if len(convDict) else "") for stmt, assign, conv, cost, depth, count, convDict in self.assignments)
         finalLines = "\n".join(str(l) for l in reversed(self.finalStmts))
         outputs = "Output vars: {" + ", ".join(str(var) + ": " + (str(conv) if len(conv) else '{}') for var, conv in self.outputs.items()) + "}"
-        return "Total cost: {:.2f}".format(self.total_cost) + "\n" + inputs + "\n" + stmts + "\n" + finalLines + "\n" + outputs + "\n"
+        return "Total cost: {:.2f}".format(self.total_cost) + "\n" + inputs + "\n" + constants + "\n" + stmts + "\n" + finalLines + "\n" + outputs + "\n"
+
+
+# function to get the required protocols for every constant
+def populateConstants(config: Config) -> None:
+    for stmt, p, conv, _, _, _, _ in config.assignments:
+        if isinstance(stmt, Assign) and isinstance(stmt.rhs, Constant):
+            val = stmt.rhs.value
+            ps = ({p} | conv) - {'_'}
+            if val not in config.constants.keys():
+                config.constants[val] = ps
+            else:
+                config.constants[val] |= ps
 
 
 # helper function to find the natural bounds of each tracked variable
@@ -1079,4 +1094,5 @@ def mix_protocols(filename: str, type_env: TypeEnv, body: list[Statement], dep_g
         if c.total_cost < minCost:
             best = c
             minCost = c.total_cost
+    populateConstants(best)
     return best
