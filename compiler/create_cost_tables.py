1ddda7836a0a626ad07e946d20504f464f96ec09
Brandon Fogg <2020bfogg@gmail.com>
2024-12-11 16:34:59 -0500

    Corrected small errors. Added error handling. Began implementing repairCostTable function

--- compiler/create_cost_tables.py
+++ compiler/create_cost_tables.py
@@ -3,6 +3,8 @@ import datetime
 from compiler.backends import Backend
 from random import random, randint
 import json
+from os import listdir
+from os.path import isfile, join
 
 
 backends = [Backend.MOTION, Backend.MP_SPDZ]
@@ -26,28 +28,68 @@ def averageStats(statsList):
 
 def genCode(backend, protocol, operator, symbol, iters, conv, vecSize):
     # TEMP CODE FOR TESTING
+    if random() < 0.2:
+        raise Exception("Random fail genCode")
     return iters*vecSize
 
 
 def runTrial(code):
     # TEMP CODE FOR TESTING
+    if random() < 0.02:
+        raise Exception("Random fail runTrial")
     return {"time": random()*2*code, "dataSent": randint(1,20*code), "commRounds": randint(1,200*code)}
 
 
 def runBenchmark(backend, protocol, operator, symbol, trials, loopIters, conv=False):
     finalStats = dict()
     for vecSize in vecSizes:
-        statsList = []
-        codeMultiIter = genCode(backend, protocol, operator, symbol, loopIters, conv, vecSize)
-        codeSingleIter = genCode(backend, protocol, operator, symbol, 1, conv, vecSize)
-        for i in range(trials):
-            statMultiIter = runTrial(codeMultiIter)
-            statSingleIter = runTrial(codeSingleIter)
-            for k, v in statSingleIter.items():
-                statMultiIter[k] -= v
-                statMultiIter[k] /= (loopIters - 1)
-            statsList.append(statMultiIter)
-        finalStats[vecSize] = averageStats(statsList)
+        statsMultiList = []
+        statsSingleList = []
+
+        # generate backend code for this benchmark
+        failGenCount = 0
+        success = False
+        while True:
+            try:
+                codeMultiIter = genCode(backend, protocol, operator, symbol, loopIters, conv, vecSize)
+                codeSingleIter = genCode(backend, protocol, operator, symbol, 1, conv, vecSize)
+                success = True
+                break
+            except:
+                failGenCount += 1
+                if failGenCount > 5:
+                    print(f"Skipping {protocol} {operator} {symbol} {vecSize} due to excessive generation errors")
+                    break
+        if not success:
+            continue
+
+        # run the benchmark
+        totalFails = 0
+        while len(statsMultiList) < trials:
+            try:
+                statsMultiList.append(runTrial(codeMultiIter))
+            except:
+                totalFails += 1
+                if totalFails > 10:
+                    break
+        while len(statsSingleList) < trials:
+            try:
+                statsSingleList.append(runTrial(codeSingleIter))
+            except:
+                totalFails += 1
+                if totalFails > 10:
+                    break
+        if len(statsMultiList) < trials or len(statsSingleList) < trials:
+            print(f"Skipping {protocol} {operator} {symbol} {vecSize} due to excessive execution errors")
+            continue
+
+        # Compute average statistics for this benchmark
+        statSingle = averageStats(statsSingleList)
+        statMulti = averageStats(statsMultiList)
+        for k, v in statSingle.items():
+            statMulti[k] -= v
+            statMulti[k] /= (loopIters - 1)
+        finalStats[vecSize] = statMulti
     return finalStats
 
 
@@ -62,8 +104,8 @@ def printOutputToJSON(outputDict, log=False, save=True):
             text_file.write(jsonData)
 
 
-def createCostTables():
-    trials, loopIters, intSize = (1000, 100, 32)
+def createCostTable():
+    trials, loopIters, intSize = (100, 1000, 32)
     resultsDict = {"params": {"trials": trials, "loopIters": loopIters, "intSize": intSize}}
     for backend in backends:
         resultsDict[str(backend)] = dict()
@@ -88,4 +130,17 @@ def createCostTables():
                     resultsDict[str(backend)][a + "2" + b] = runBenchmark(backend, a + "2" + b, None, None, trials, loopIters, conv=True)
     printOutputToJSON(resultsDict, log=False, save=True)
 
-createCostTables()
+
+def repairCostTable(tableName="", useLastTable=True):
+    if useLastTable:
+        fileNames = sorted([f for f in listdir() if isfile(f) and f.__contains__('cost_table.txt')])
+        if not len(fileNames):
+            print("No cost tables found to repair")
+            return
+        tableName = fileNames[-1]
+    with open(tableName) as json_data:
+        resultsDict = json.load(json_data)
+    pass
+
+# createCostTable()
+repairCostTable()
